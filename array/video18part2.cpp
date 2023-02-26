0acd91f3f8862e0e8f4d54b7848e204ac0814afc
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-02-26 14:57:28 +0530

    max and min no hardest for me to solve

---
+++ array/video18part2.cpp
@@ -0,0 +1,26 @@
+//find the maximum and minimum element of from the array
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+  for(int i=0;i<n;i++)
+     {
+         cin>>arr[i];
+     }
+
+     int maxNo= INT_MIN;
+     int minNo= INT_MAX;
+
+     for(int i=0;i<n;i++)
+     {
+        maxNo= max(maxNo,arr[i]);
+        minNo= min(minNo,arr[i]);
+     }
+     cout << "Maximum Number= "<<maxNo<<endl;
+     cout << "Minimum Number= "<<minNo;
+     return 0;
+}
