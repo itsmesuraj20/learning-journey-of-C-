38aa636955ce9900a07f85ba2fd0c8a2961322d8
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-02-26 19:30:59 +0530

    linear search and binary search learning from APCO

---
+++ searching/linearsearch.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Linear(int arr[],int n,int k)
+{
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        if(k==arr[i])
+        {
+        return i; 
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<Linear(arr,n,k);
+    return 0;
+}
