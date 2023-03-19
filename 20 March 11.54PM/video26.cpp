eaa93a0ee514ccdd01517e36ba93ffec14cea03d
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-03-20 00:14:15 +0530

    Two dimensional array learning

---
+++ 20 March 11.54PM/video26.cpp
@@ -0,0 +1,52 @@
+//Two Dimensional 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m,target;
+    cin>>n>>m>>target;
+    int a[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int  j = 0; j < m; j++)
+        {
+            cin>>a[i][j];
+        }    
+    }
+    cout<<endl; 
+    for (int i = 0; i < n; i++)
+    {
+        for (int  j = 0; j < m; j++)
+        {
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    bool flag=false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if(a[i][j]==target)
+            {
+                cout<<i<<" "<<j<<endl;
+                flag=true;
+            }
+
+        }
+        
+    }
+    if(flag)
+    {
+        cout<<"Element Found in the array Elements"<<" ";
+    }
+    else
+    {
+        cout<<"Not found";
+    }
+    
+
+
+    return 0;
+}
