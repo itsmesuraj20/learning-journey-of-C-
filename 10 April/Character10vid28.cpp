79a5945999c4f9f9563fea416302308a9ec65b6d
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-04-10 01:43:38 +0530

    Learning Character array Vid-28 on Apr 10

---
+++ 10 April/Character10vid28.cpp
@@ -0,0 +1,17 @@
+// Introducing to Character in Arrays;
+//Traversing the array using character array
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  char arr[100]="apple";
+  int i=0;
+    while (arr[i]!='\0')
+    {
+        cout<<arr[i]<< " ";
+        i++;
+    }
+    return 0;
+}
+     
--- 5 April/check.cpp
+++ 5 April/check.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
-    
-
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 5; i++)
     {
-        cout<< i<<") "<<"Hello World"<<endl;;
+        for (int j = 0; j < 4; j++)
+        {
+            cout<<"*"<<" ";
+        }
+        
+        cout<<endl;;
     }
     return 0;
 }
