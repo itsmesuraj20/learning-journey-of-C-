e988523e1c5af3621cda44cbebee4f2b199923c0
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-05-01 14:30:17 +0530

    Learning String from Apna College

---
+++ 1 May/StringVid31.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str="Eastern X HI This is \n Me suraj singh 20 " ;
+    cout<<str; 
+}
---
+++ 15 April/character.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+    char arr[num];
+    cin.getline(arr,num);
+
+    cout<<arr;
+
+    return 0;
+}
