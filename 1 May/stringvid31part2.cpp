d5c5b6cdc8c4671f364d9ac4962dd0be718f44b1
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-05-01 21:32:20 +0530

    Sorted done string

---
+++ 1 May/stringvid31part2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s1="15355089";
+    // // n=s1.length();
+    // // for (int i = 0; i < n; i++)
+    // // {
+    // //     cout<<s1[i]<<endl;
+    // // }
+    // int n=stoi(s1);
+    // cout<<n;
+    string s1="xyzabc";
+    sort(s1.begin(),s1.end());
+    cout<<s1<<endl;
+    return 0;
+}
