1a7d7287bfbd88dd49b5b0c97d7fb2c6f6fe07fc
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-05-02 07:41:08 +0530

    TODAY LEARNED ABOUT CHARACTER ARRAY AND STARTED POINTER

---
+++ 2 May/Pointer.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int *ap;
+    ap=&a;
+    
+    cout<<&a<<endl;
+    cout<<ap<<endl;
+    
+    cout<<*ap<<endl;
+}
